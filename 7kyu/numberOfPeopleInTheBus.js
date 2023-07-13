a19d965db4837079a4951aa840e4c5714263382d
hadsome-Droid <xkumbhax@gmail.com>
2023-07-13 09:40:58 +0300

    Number of People in the Bus

--- 7kyu/numberOfPeopleInTheBus.js
+++ 7kyu/numberOfPeopleInTheBus.js
@@ -12,3 +12,7 @@ const number = (busStops) => {
    return countPerson
 
 }
+
+console.log(number([[10, 0], [3, 5], [5, 8]]))
+console.log(number([[3, 0], [9, 1], [4, 10], [12, 2], [6, 1], [7, 10]]))
+console.log(number([[0, 0]]))
