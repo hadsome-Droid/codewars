45daa17efe87446b5b323ce28bfc6beef5d16047
hadsome-Droid <xkumbhax@gmail.com>
2023-07-22 08:37:34 +0300

    convertBooleanToString

---
+++ 8kyu/convertBooleanToString.js
@@ -0,0 +1,15 @@
+//todo                 Convert a Boolean to a String
+
+
+//$ Реализуйте функцию, которая преобразует заданное логическое
+//$  значение в его строковое представление.
+
+//$ Примечание: Будут указаны только допустимые входные данные.
+
+function booleanToString(b) {
+   return String(b)
+}
+
+
+
+console.log(booleanToString(false))
