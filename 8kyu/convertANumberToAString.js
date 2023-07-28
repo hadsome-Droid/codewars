864d0fbb13342ed0fe0d7440f022cbe56bffa262
hadsome-Droid <xkumbhax@gmail.com>
2023-07-28 09:00:37 +0300

    convertANumberToAString

---
+++ 8kyu/convertANumberToAString.js
@@ -0,0 +1,11 @@
+//todo                 Convert a Number to a String!
+
+//*  Нам нужна функция, которая может преобразовывать число(integer) в строку.
+
+//*  Какие способы достижения этого вы знаете ?
+
+function numberToString(num) {
+   return num.toString();
+}
+
+console.log(numberToString(67))
