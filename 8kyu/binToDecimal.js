b99bcfd7ff6d075223bec75a2065e1a6d3a3936e
hadsome-Droid <xkumbhax@gmail.com>
2023-08-05 09:37:44 +0300

    binToDecimal

---
+++ 8kyu/binToDecimal.js
@@ -0,0 +1,12 @@
+// todo                         Bin to Decimal
+
+
+//* Завершите функцию, которая преобразует двоичное число(заданное в виде строки)
+//*  в десятичное число.
+
+
+function binToDec(bin) {
+   return parseInt(bin, 2);
+}
+
+console.log(binToDec('1'))
